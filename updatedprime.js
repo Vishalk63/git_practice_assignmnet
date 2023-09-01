af69080a7dee48b154c4c0e6a383d037e0ee0709
Vishal Kachare <vishalkachare323@gmail.com>
2023-09-01 11:46:00 +0530

    updated prime from vishal

---
+++ updatedprime.js
@@ -0,0 +1,16 @@
+
+// prime code
+
+let num=31;
+let count = 0;
+for(i=0;i<num;i++){
+    if(num%i==0){
+        count++;
+    }
+}
+if(count==2){
+    console.log(num,"is prime")
+}
+else{
+    console.log(num,"is not prime")
+}
